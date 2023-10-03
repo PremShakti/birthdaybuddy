6796a949f19dce2c2ec3f431dff263cc758c4707
PremShakti <premshakti.ps@gmail.com>
2023-10-03 09:08:52 +0530

    b

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -68,6 +68,7 @@ import { useParams } from 'react-router-dom';
 const BirthdayEffect = () => {
     let { name } = useParams();
   return (
+    <>
     <div className="birthday-message">
         <h1>
         <span className="happy-birthday">Happy Birthday {name} !</span>
@@ -83,6 +84,8 @@ const BirthdayEffect = () => {
       <div className="confetti"></div>
       <div className="confetti"></div>
     </div>
+    <div className='balloon1'><img src="/bp1.png" alt="happy birthday" /></div>
+    </>
   );
 };
 
